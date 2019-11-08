2b56931f1d0bbb9982e85cb862a8313b243bd12f
News Nation <admin@itnewsnation.onmicrosoft.com>
2019-11-08 19:25:44 +0530

    test test 123 class2

--- Console_NN/Program.cs
+++ Console_NN/Program.cs
@@ -10,19 +10,37 @@ namespace Console_NN
     {
         static void Main(string[] args)
         {
-            string[] Names = { "ABC", "BCD", "CDEA", "DEF", "EFG" };
-            string value = "Yes";
-            while (value.ToUpper() == "YES")
-            {
-                Console.WriteLine("Please enter one character");
-                string str = Console.ReadLine();
-                var list = from x in Names where x.Contains(str) select x;
-                foreach (string x in list)
-                    Console.WriteLine(x);
+            Console.WriteLine("Student List : ");
+            List<Student> List = new List<Student>();
+            List.Add(new Student { StudentID = 1, StudentName = "Bipin", Age = 31 });
+            List.Add(new Student { StudentID = 2, StudentName = "Rohit", Age = 32 });
+            List.Add(new Student { StudentID = 3, StudentName = "Virat", Age = 27 });
+            List.Add(new Student { StudentID = 4, StudentName = "Suresh", Age = 34 });
+            List.Add(new Student { StudentID = 5, StudentName = "Rishabh", Age = 21 });
+            List.Add(new Student { StudentID = 6, StudentName = "Sachin", Age = 40 });
+            List.Add(new Student { StudentID = 7, StudentName = "Bipin Bhatt", Age = 32 });
+            List.Add(new Student { StudentID = 8, StudentName = "Ravi", Age = 32 });
+            List.Add(new Student { StudentID = 9, StudentName = "Ankul", Age = 32 });
+            List.Add(new Student { StudentID = 10, StudentName = "Ramesh", Age = 35 });
+            List.Add(new Student { StudentID = 11, StudentName = "Laxman", Age = 45 });
 
-                Console.WriteLine("Do you want to continue : Yes or No !");
+            foreach (Student obj in List)
+                Console.WriteLine(" Student ID :" + obj.StudentID + " Student Name :" + obj.StudentName + " Student Age :" + obj.Age);
+
+            string value = string.Empty;
+            do
+            {
+                Console.Write("Search Name : ");
+                string str = Console.ReadLine();
+
+                var list = from x in List where x.StudentName.ToLower().Contains(str.ToLower()) select x;
+
+                foreach (Student obj in list)
+                    Console.WriteLine(" Student ID :" + obj.StudentID + " Student Name :" + obj.StudentName + " Student Age :" + obj.Age);
+
+                Console.Write("Do you want to continue : ");
                 value = Console.ReadLine();
-            }
+            }while (value.ToUpper() == "YES" || value.ToUpper() == "Y");
 
 
 
@@ -39,10 +57,6 @@ namespace Console_NN
         public String StudentName { get; set; }
         public int Age { get; set; }
 
-        public Student()
-        {
-            StudentID = 0;
-        }
     }
 
 }
